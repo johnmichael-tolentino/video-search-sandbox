c1d2486e44dfffd3a493b4df76fe7c391984edb7
jolentino <johnm.tolentino@gmail.com>
2021-02-03 23:09:04 -0500

    Update Google API key

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // Youtube API KEY.
 // Capitalized because it is a constant variable and will not be changed.
 // It can be accessed by other people if this project is public.
-const KEY = 'AIzaSyBeGXPzvwleJ_SfMBz6iAQZ6z-nksne7yo';
+const KEY = 'AIzaSyCc0rTwKIDHvF5Ro7VvU8IEYgeoSJek4SI';
 
 //  Creating and exporting a preconfigured Axios client for Youtube API.
 export default axios.create({
